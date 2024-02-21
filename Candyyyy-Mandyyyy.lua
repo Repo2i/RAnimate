fb5528e936f299001403e67eebf0a80a80aa19bc
Sssssssqw <159781855+Repo2i@users.noreply.github.com>
2024-02-21 14:53:13 +0000

    w

--- Candyyyy-Mandyyyy.lua
+++ Candyyyy-Mandyyyy.lua
@@ -181,7 +181,7 @@ local Table = Emperean.Start({
 		Enabled = true,
 		ToolFling = true,
 		Timeout = 1.25,
-		Velocity = Vector3new(0, 4096, 4096),
+		Velocity = Vector3new(3339, 4096, 4096),
 		HighlightTargets = true
 	}
 })
@@ -195,7 +195,7 @@ local backpack = player:WaitForChild("Backpack") -- Get the player's backpack
 
 local function reEquipAllTools()
     for _, item in ipairs(backpack:GetChildren()) do
-        if item:IsA("Boombox") then
+        if item:IsA("Tool") then
             player.Character.Humanoid:EquipTool(item)
             wait(1) 
         end
